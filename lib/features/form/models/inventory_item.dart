21d1a3da1d4df7d9d11c03d3a479d5903b9c3759
reksee-firewall <ramonfernando@id.uff.br>
2024-11-15 17:37:31 -0300

    Update: Implementação da página de formulário.

---
+++ lib/features/form/models/inventory_item.dart
@@ -0,0 +1,54 @@
+import 'package:intl/intl.dart';
+
+class InventoryItem {
+  final String barcode;
+  final String name;
+  final String? description;
+  final String? packageId;
+  final String location;
+  final List<String>? images;
+  final String? geolocation;
+  final String? observations;
+  final DateTime date;
+
+  InventoryItem({
+    required this.barcode,
+    required this.name,
+    this.description,
+    this.packageId,
+    required this.location,
+    this.images,
+    this.geolocation,
+    this.observations,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'barcode': barcode,
+      'name': name,
+      'description': description,
+      'packageId': packageId,
+      'location': location,
+      'images': images,
+      'geolocation': geolocation,
+      'observations': observations,
+      'date': DateFormat('yyyy-MM-dd').format(date),
+    };
+  }
+
+  factory InventoryItem.fromMap(Map<String, dynamic> map) {
+    return InventoryItem(
+      barcode: map['barcode'] ?? '',
+      name: map['name'] ?? '',
+      description: map['description'],
+      packageId: map['packageId'],
+      location: map['location'] ?? '',
+      images: List<String>.from(map['images'] ?? []),
+      geolocation: map['geolocation'],
+      observations: map['observations'],
+      date: DateFormat('yyyy-MM-dd')
+          .parse(map['date'] ?? DateTime.now().toString()),
+    );
+  }
+}
