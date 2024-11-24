7ab32972ab59cc573bbe1cbfc8b29504bfe03dc4
reksee-firewall <ramonfernando@id.uff.br>
2024-11-23 22:21:59 -0300

    Minor Update: Tags filter on PackageListWidget.

--- lib/features/home/views/package_list_widget.dart
+++ lib/features/home/views/package_list_widget.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stiuffcoletorinventario/core/models/inventory_item.dart';
 import 'package:stiuffcoletorinventario/core/models/package_model.dart';
 import 'package:stiuffcoletorinventario/core/providers/inventory_provider.dart';
+import 'package:stiuffcoletorinventario/features/home/controllers/tag_filter_controller.dart';
 import 'package:stiuffcoletorinventario/shared/utils/app_colors.dart';
 import 'package:intl/intl.dart';
 
@@ -104,10 +107,48 @@ class _PackageListWidgetState extends State<PackageListWidget> {
     return groupedPackages;
   }
 
+  void _showFilterModal(BuildContext context, List<String> allTags) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Consumer<TagFilterController>(
+          builder: (context, tagFilterController, child) {
+            return AlertDialog(
+              title: const Text("Filtrar por Tags"),
+              content: SingleChildScrollView(
+                child: Wrap(
+                  spacing: 10.0,
+                  runSpacing: 10.0,
+                  children: allTags.where((tag) => tag.isNotEmpty).map((tag) {
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: tagFilterController.isTagSelected(tag),
+                      onSelected: (isSelected) {
+                        tagFilterController.toggleTag(tag);
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Fechar"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<InventoryProvider>(
-      builder: (context, inventoryProvider, child) {
+    return Consumer2<InventoryProvider, TagFilterController>(
+      builder: (context, inventoryProvider, tagFilterController, child) {
         final packages = inventoryProvider.sentPackages;
         final packagesItemsMap = inventoryProvider.packagesItemsMap;
 
@@ -121,49 +162,21 @@ class _PackageListWidgetState extends State<PackageListWidget> {
               }) ??
               false;
 
-          return matchesPackageName || matchesItemName;
-        }).toList();
+          final matchesTags = tagFilterController.selectedTags.isEmpty ||
+              package.tags
+                  .any((tag) => tagFilterController.selectedTags.contains(tag));
 
-        if (filteredPackages.isEmpty) {
-          return const Center(
-            child: Text(
-              "Nenhum pacote encontrado.",
-              style: TextStyle(fontSize: 16.0, color: Colors.grey),
-            ),
-          );
-        }
+          return (matchesPackageName || matchesItemName) && matchesTags;
+        }).toList();
 
         final sortedPackages = _sortPackagesByCreatedAt(filteredPackages);
         final groupedPackages = _groupPackagesByMonth(sortedPackages);
 
+        final allTags =
+            packages.expand((package) => package.tags).toSet().toList();
+
         return Column(
           children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (query) {
-                        setState(() {
-                          _searchQuery = query;
-                        });
-                      },
-                      decoration: const InputDecoration(
-                        labelText: 'Pesquisar Pacote ou Item',
-                        border: OutlineInputBorder(),
-                        suffixIcon: Icon(Icons.search),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.filter_list),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ),
             const Padding(
               padding: EdgeInsets.only(top: 16, left: 16.0, bottom: 16.0),
               child: Align(
@@ -178,57 +191,151 @@ class _PackageListWidgetState extends State<PackageListWidget> {
                 ),
               ),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: groupedPackages.keys.length,
-              itemBuilder: (context, index) {
-                final monthYear = groupedPackages.keys.elementAt(index);
-                final monthPackages = groupedPackages[monthYear]!;
-
-                return ExpansionTile(
-                  title: Text(
-                    monthYear,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 16.0, right: 16.0, bottom: 16.0, top: 16.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(5.0),
+                              ),
+                            ),
+                            child: TextField(
+                              onChanged: (query) {
+                                setState(() {
+                                  _searchQuery = query;
+                                });
+                              },
+                              textAlign: TextAlign.start,
+                              decoration: const InputDecoration(
+                                hintText: 'Pesquisar Pacote ou Item',
+                                prefixIcon: Icon(Icons.search),
+                                border: InputBorder.none,
+                                contentPadding:
+                                    EdgeInsets.symmetric(vertical: 12.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: const Icon(Icons.filter_list),
+                              color: Colors.white,
+                              onPressed: () {
+                                _showFilterModal(context, allTags);
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  children: monthPackages.map((PackageModel package) {
-                    final items = packagesItemsMap[package.id] ?? [];
-                    final formattedDate = DateFormat('dd/MM/yyyy HH:mm').format(
-                        package.createdAt ??
-                            DateTime.fromMillisecondsSinceEpoch(0));
+                  if (filteredPackages.isEmpty) ...[
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Text(
+                          "Nenhum pacote encontrado! =(",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                  (filteredPackages.isNotEmpty)
+                      ? Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: groupedPackages.keys.length,
+                            itemBuilder: (context, index) {
+                              final monthYear =
+                                  groupedPackages.keys.elementAt(index);
+                              final monthPackages = groupedPackages[monthYear]!;
 
-                    return Card(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 16.0),
-                      child: ExpansionTile(
-                        title:
-                            Text("Pacote: ${package.name} (ID: ${package.id})"),
-                        subtitle: Text(
-                            "Tags: ${package.tags.join(', ')}\nEnviado em: $formattedDate"),
-                        children: items.isEmpty
-                            ? [
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Nenhum item associado a este pacote.",
-                                    style: TextStyle(color: Colors.grey),
+                              return ExpansionTile(
+                                title: Text(
+                                  toBeginningOfSentenceCase(monthYear) ??
+                                      monthYear,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 22,
                                   ),
                                 ),
-                              ]
-                            : items.map((item) {
-                                return ListTile(
-                                  title: Text(item.name),
-                                  subtitle:
-                                      Text("Código de Barras: ${item.barcode}"),
-                                  leading: const Icon(Icons.inventory),
-                                  onTap: () => _showItemDetails(context, item),
-                                );
-                              }).toList(),
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
+                                children:
+                                    monthPackages.map((PackageModel package) {
+                                  final items =
+                                      packagesItemsMap[package.id] ?? [];
+                                  final formattedDate = DateFormat(
+                                          'dd/MM/yyyy HH:mm')
+                                      .format(package.createdAt ??
+                                          DateTime.fromMillisecondsSinceEpoch(
+                                              0));
+
+                                  return Card(
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 16.0),
+                                    child: ExpansionTile(
+                                      title: Text(
+                                          "Pacote: ${package.name} (ID: ${package.id})"),
+                                      subtitle: Text(
+                                          "Tags: ${package.tags.join(', ')}\nEnviado em: $formattedDate"),
+                                      children: items.isEmpty
+                                          ? [
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  "Nenhum item associado a este pacote.",
+                                                  style: TextStyle(
+                                                    color: Theme.of(context)
+                                                        .primaryColor,
+                                                  ),
+                                                ),
+                                              ),
+                                            ]
+                                          : items.map((item) {
+                                              return ListTile(
+                                                title: Text(item.name),
+                                                subtitle: Text(
+                                                    "Código de Barras: ${item.barcode}"),
+                                                leading:
+                                                    const Icon(Icons.inventory),
+                                                onTap: () => _showItemDetails(
+                                                    context, item),
+                                              );
+                                            }).toList(),
+                                    ),
+                                  );
+                                }).toList(),
+                              );
+                            },
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                ],
+              ),
             ),
           ],
         );
